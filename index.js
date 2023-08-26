eeee8275b45d62d772da2a2bcc49344b351dd0b1
Javakios <dinos18kostas@gmail.com>
2023-08-26 18:39:39 +0300

    routes,middlewares and some database drivers functionality

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-// APP TEMPLATE 
+// FILE TEMPLATES 
 const appTemplate = require('./lib/templates/app.template');
+const userRoutesTemplate = require('./lib/templates/usersRoute.template');
+const create_middleware = require('./lib/templates/create_middleware.template');
+const update_middleware = require('./lib/templates/update_middleware');
 
 // TERMINAL UI 
 const readLine = require('readline');
 
+// DATABASE DRIVERS 
+const drivers = require('./lib/drivers/available.drivers');
+
+//COLORS
 const colors = {
     reset: '\x1b[0m',
     green: '\x1b[32m'
@@ -31,7 +38,7 @@ const main = async () => {
     console.log('Creating Express.js project: Loading...');
 
     rl.question('Enter project name: ', (projectName) => {
-        rl.close();
+
         if (!projectName.trim()) {
             console.log('Project name cannot be empty.');
             return;
@@ -40,42 +47,102 @@ const main = async () => {
         // Show loading message while creating the project directory
         console.log('Creating project directory: Loading...');
         createDirectory(projectName);
-
-        // Navigate to the project directory
-        process.chdir(projectName);
-
-        // Show loading message while initializing npm
-        console.log('Initializing npm: Loading...');
-        executeCommand('npm init -y');
-
-        // Show loading message while installing Express.js
-        console.log('Installing Express.js: Loading...');
-        executeCommand('npm i --save express');
-
-        // Show loading message while installing body-parser
-        console.log('Installing body-parser: Loading...');
-        executeCommand('npm i --save body-parser');
-
-        // Show loading message while installing Nodemon
-        console.log('Installing Nodemon: Loading...');
-        executeCommand('npm i --save-dev nodemon');
-
-        // Show loading message while installing Morgan
-        console.log('Installing Morgan: Loading ...');
-        executeCommand('npm i --save-dev morgan');
-
-        // Add the start script
-        console.log('Adding start script: Loading...');
-        addStartScript('./package.json', process.cwd());
+        // Ask for port 
+        rl.question('Enter the port number (default:3000): ', (port) => {
+            console.log('Choose a database driver:');
+            drivers.forEach((driver, index) => {
+                console.log(`${index + 1}) ${driver}`);
+            });
+            rl.question('Enter your database driver: ', (choice) => {
 
 
-        // Show loading message while creating the app.js file
-        console.log('Creating app.js: Loading...');
-        createFile('app', appTemplate, '.js');
+                rl.close();
 
-        // Show completion message
-        console.log(`${colors.green}Express.js project generated successfully✔${colors.reset}`);
-    });
+
+                // Navigate to the project directory
+                try {
+                    process.chdir(projectName);
+                } catch (error) {
+                    console.error('Error navigating to project directory:', error.message);
+                    return;
+                }
+                // Show loading message while initializing npm
+                console.log('Initializing npm: Loading...');
+                executeCommand('npm init -y');
+
+                // Show loading message while installing Express.js
+                console.log('Installing Express.js: Loading...');
+                executeCommand('npm i --save express');
+
+                // Show loading message while installing body-parser
+                console.log('Installing body-parser: Loading...');
+                executeCommand('npm i --save body-parser');
+
+                // Show loading message while installing the database driver 
+                console.log(`Installing ${drivers[choice]}: Loading...`);
+                executeCommand(`npm i --save ${drivers[choice]}`);
+
+                //Show loading message while installing express-validator
+                console.log('Installing express-validator : Loading...')
+                executeCommand('npm i --save express-validator');
+
+                // Show loading message while installing Nodemon
+                console.log('Installing Nodemon: Loading...');
+                executeCommand('npm i --save-dev nodemon');
+
+                // Show loading message while installing Morgan
+                console.log('Installing Morgan: Loading...');
+                executeCommand('npm i --save-dev morgan');
+
+                // Add the start script
+                console.log('Adding start script: Loading...');
+                addStartScript('./package.json', process.cwd());
+
+
+                // Show loading message while creating the app.js file
+                console.log('Creating app.js: Loading...');
+                createFile('app', appTemplate(port), '.js');
+
+                // Show loading message while creating the models directory
+                console.log('Creating models : Loading...');
+                createDirectory('models');
+
+                // Show loading message while creating the user model file
+                console.log('Creatgin user model : Loading...');
+                createFile('user', '', '.js', 'models');
+
+                // Show loading message while crating the routes directory
+                console.log('Creating routes : Loading...');
+                createDirectory('routes');
+
+                //Show Loading message while creating the users routes file
+                console.log('Creating users.js router: Loading...');
+                createFile('users', userRoutesTemplate, '.js', 'routes');
+
+
+                //Show loading message while creating the controllers direcotory
+                console.log('Creating controllers : Loading...');
+                createDirectory('controllers');
+
+                console.log('Creating middleware : Loading...');
+                createDirectory('middleware');
+
+                console.log('Creating users middleware directory : Loading...');
+
+                createDirectory('middleware/users');
+
+                // Show loading message while creating the user's midlewares
+                console.log('Creating users middleware files : Loading...')
+                createFile('create', create_middleware, '.js', 'middleware/users')
+                createFile('update', update_middleware, '.js', 'middleware/users')
+
+                console.log(`${colors.green}User's Middlewares created succesfully${colors.reset}`);
+
+                // Show completion message
+                console.log(`${colors.green}Express.js project generated successfully✔${colors.reset}`);
+            });
+        })
+    })
 };
 
 main();
